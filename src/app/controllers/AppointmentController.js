8d0f2710548127c65c01cd7f871027b10d7a8c69
dvsilva <dvsilva@gmail.com>
2019-11-18 20:37:11 -0300

    aula 6

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -26,6 +27,31 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    /**
+     * Check for past dates
+     */
+    // parseISO retorna objeto data do javascript
+    // startOfHour retorna hora redonda
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    /**
+     * Check date avaiability
+     */
+    const checkAvaiability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if(checkAvaiability){
+      return res.status(400).json({ error: 'Appointment date is not available' });
     }
 
     const appointment = await Appointment.create({
