5324d10eabe54ce4e409191af2643d55a99bbbbc
dvsilva <dvsilva@gmail.com>
2019-11-20 07:45:04 -0300

    aula 15

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false, // se está usando ssl
+  auth: {
+    user: '576da455d58038',
+    pass: 'b4b56d582565bc',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
