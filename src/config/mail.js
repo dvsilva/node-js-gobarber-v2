ff31766dc1d38ff56b593c1707e9094169840216
dvsilva <dvsilva@gmail.com>
2019-11-20 11:15:14 -0300

    aula 22

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false, // se está usando ssl
   auth: {
-    user: '576da455d58038',
-    pass: 'b4b56d582565bc',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://4e48a6c18fdf45d2a3fe1758f9ecc3bc@sentry.io/1827855',
+  dsn: process.env.SENTRY_DSN,
 };
