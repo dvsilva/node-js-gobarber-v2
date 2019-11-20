bc50ac04d807dc3253a2b4ab216c1ef0dfe7e141
dvsilva <dvsilva@gmail.com>
2019-11-20 10:49:59 -0300

    aula 21

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 import express from 'express';
+import 'express-async-errors'; // importar antes das rotas
+import Youch from 'youch';
+
 import routes from './routes';
 import './database';
 import path from 'path';
+
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    // sentry - The request handler must be the first middleware on the app
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.server.use(express.json());
     // permitir visualizar imagens dos avatars
     this.server.use(
@@ -22,6 +34,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // express entende que métodos que recebem 4 parametros é middleware de exceção
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON(); // .toHTML()
+      return res.status(500).json(errors);
+    });
   }
 }
 
