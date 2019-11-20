18ff60db55d9e9c9e15ad2aa2883056234292111
dvsilva <dvsilva@gmail.com>
2019-11-20 06:54:03 -0300

    aula 12

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -1,10 +1,5 @@
-import * as Yup from 'yup';
 import User from '../models/User';
-import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
-
-import { startOfDay, endOfDay, parseISO } from 'date-fns';
-import { Op } from 'sequelize';
 
 class NotificationController {
   async index(req, res) {
@@ -17,9 +12,6 @@ class NotificationController {
         .status(401)
         .json({ error: 'Only providers can load notifications' });
     }
-
-    const { date } = req.query;
-    const parsedDate = parseISO(date);
 
     // sequelize - findAll | mongoose - find
     const notifications = await Notification.find({
